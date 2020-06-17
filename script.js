22ab72ab443d8830366c4b68ae53784b3b678e5f
cwi <GeekMonster842@gmail.com>
2020-06-17 18:17:49 +0000

    css

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
+
 document.querySelectorAll('.highlight').forEach(function(node) {
-    node.innerHTML = "<img src='https://img.icons8.com/windows/30/999999/copy.png'>" + node.innerHTML
+  node.innerHTML = "<img class='copycode' src='https://img.icons8.com/windows/30/999999/copy.png'>" + node.innerHTML
 });
+
+$(document).on("click",":header", function () {
+   var clickedBtnID = $(this).attr('id'); // or var clickedBtnID = this.id
+   window.location.hash = '#' + clickedBtnID;
+});
+
+$(document).on("click",".copycode", function () {
+   copyToClipboard($(this).parent().text());
+	 alert("Code is copied to clipboard.");
+});
+
+window.onhashchange = function(){
+	var y = $(window).scrollTop();  //your current y position on the page
+	$(window).scrollTop(y-50);
+}
+
+window.onload = function(){
+	if (window.location.hash != ""){
+		var y = $(window).scrollTop();  //your current y position on the page
+		$(window).scrollTop(y-50);
+	}
+}
