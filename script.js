131cc8cb7e65b5aacc4a0ce4973ebde1b65a0cf4
cwi <GeekMonster842@gmail.com>
2020-07-08 02:43:05 +0000

    cookies!!!!! :cookie:

--- script.js
+++ script.js
@@ -89,3 +89,13 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
