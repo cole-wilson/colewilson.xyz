7a2dbb9c2ff559be0cd98620e12df69c7757bc63
cwi <GeekMonster842@gmail.com>
2020-07-08 02:39:46 +0000

    Dark Mode!!!!!!!!!

--- script.js
+++ script.js
@@ -76,3 +76,16 @@ var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, 
 if (height < viewportHeight+1000) {
 	$('footer').css({'position':'fixed',"bottom":"0"})
 }
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
