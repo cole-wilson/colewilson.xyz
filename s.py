b39225f039e94709279489f4f857e5e3570f28d7
cole-wilson <49500253+cole-wilson@users.noreply.github.com>
2020-08-06 10:36:29 -0700

    Update s.py

--- s.py
+++ s.py
@@ -8,13 +8,13 @@ http://colewilson.xyz/* https://colewilson.xyz/:splat 301!
   {% if url.countries or url.languages %}
   
   {% for country in url.countries %}
-/u/{{url.key}} {{url.u}} 302! Country={{url.country}}
+/u/{{url.key}} {{url.u}} 302 Country={{url.country}}
   {% endfor %}
   {% for language in url.languages %}
-/u/{{url.key}} {{url.u}} 302! Language={{url.language}}
+/u/{{url.key}} {{url.u}} 302 Language={{url.language}}
   {% endfor %}
   {% else %}
-/u/{{url.key}} {{url.u}} 302!
+/u/{{url.key}} {{url.u}} 302
   {% endif %}
   
 {% endfor %}
