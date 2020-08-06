73cb6540bd985fa013795267884eb169e41a2fd8
cole-wilson <49500253+cole-wilson@users.noreply.github.com>
2020-08-06 08:15:56 -0700

    Update build.py

--- build.py
+++ build.py
@@ -1,15 +1,18 @@
-import random, os
+---
+description: prebuild
+---
+import random, os, yaml
 
 choices = [
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  ""
+  "a",
+  "a",
+  "a",
+  "a",
+  "a",
+  "a",
+  "a",
+  "a",
+  "a"
 ]
 
-os.system("sed -i 's/BOB???/" + random.choice(choices) + "/g' netlify.toml")
+os.system("sed -i 's/BOB???/" + random.choice(choices) + "{{page.description}}/g' netlify.toml")
