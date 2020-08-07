950d2f331e15aa871d773689db6172ffcbe4c139
cole-wilson <49500253+cole-wilson@users.noreply.github.com>
2020-08-07 11:20:02 -0700

    Create build.py

---
+++ build.py
@@ -0,0 +1,37 @@
+import os, json, requests
+os.system('curl -u cole-wilson:12d0e4cca5a9bc2c5eb3cd40999b231b4bd8e5df -s "https://api.github.com/users/cole-wilson/repos?access_token=[12d0e4cca5a9bc2c5eb3cd40999b231b4bd8e5df]" > output.json')
+
+data = json.loads(open('output.json').read())
+
+a = """---
+title: {title}
+date: {datee}
+desc: {desc}
+image: {image}
+site: {site}
+repo: {repo}
+user: {user}
+download: {download}
+---
+{body}
+"""
+
+for x in data:
+	if not os.path.isfile('_projects/' + x['name'] + '.md'):		
+		os.system('touch _projects/' + x['name'] + '.md')
+		f = open('_projects/' + x['name'] + '.md','w+')
+		b = str(requests.get('https://raw.githubusercontent.com/'+x['owner']['login']+'/'+x['name']+'/master/README.md').content.decode('unicode_escape'))
+		if b == '404: Not Found':
+			b = str(x['description'])
+		f.write(a.format(
+			title=x['name'],
+			datee=x['created_at'],
+			desc="\"" + str(x['description']) + "\"",
+			image="/img/black.jpg",
+			site=x['homepage'],
+			repo=x['name'],
+			user=x['owner']['login'],
+			download='',
+			body=b
+		))
+		f.close()
