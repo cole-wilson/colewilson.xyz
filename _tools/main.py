ecf9ffffadcbb2592b6b7ef5bf173aae8df26788
cole-wilson <49500253+cole-wilson@users.noreply.github.com>
2020-08-13 12:35:48 -0700

    Create main.py

---
+++ _tools/main.py
@@ -0,0 +1,45 @@
+import hashlib, mincss, os, re
+
+bigfile = ''
+
+f = re.findall('\@import url\(\'(.*)\'\)',open('_assets/css/main.css').read())
+for x in f:
+	if os.path.isfile('_assets/css/'+x):
+		bigfile=open('_assets/css/'+x).read()+bigfile
+	else:
+		os.system('wget '+x+' -O temp')
+		bigfile=open('temp').read()+bigfile
+		os.system('rm -rf temp')
+bigfile=open('_assets/css/main.css').read()+bigfile
+
+def hash_file(filename):
+	h = hashlib.sha1()
+	with open(filename,'rb') as file:
+		chunk = 0
+		while chunk != b'':
+			chunk = file.read(1024)
+			h.update(chunk)
+	return h.hexdigest()
+
+os.system('rm -rf assets/;mkdir assets')
+open('assets/bundle.min.css','w+').write(mincss.cssmin(bigfile))
+hash = hash_file('assets/bundle.min.css')
+os.system('mv assets/bundle.min.css assets/'+hash+'.min.css')
+
+for path, directories, files in os.walk('.'):
+	for file in files:
+		if '.html' in file:
+			print(path+'/'+file)
+			os.system("sed -i 's/{% css main %}/\<link rel\='stylesheet' href\='"+hash+".min.css'\>\<\/link\>/g' "+path+'/'+file)
+
+hash = hash_file('_assets/js/script.js')
+
+os.system('mv _assets/js/script.js assets/'+hash+'.js')
+
+for path, directories, files in os.walk('.'):
+	for file in files:
+		if '.html' in file:
+			print(path+'/'+file)
+			os.system("sed -i 's/{% js main %}/\<script src\='"+hash+".js'\>\<\/script\>/g' "+path+'/'+file)
+
+os.system('mv _assets/icons/* .')
