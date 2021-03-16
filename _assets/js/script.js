9281ea9fd9f131c6009ae2ed4a8f9086980b3594
Cole Wilson <cole@colewilson.xyz>
2021-03-16 11:36:41 -0700

    Update script.js

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -35,7 +35,7 @@ $("#openav").click(function(){
 
 var newScript = document.createElement("script");
 newScript.src = atob("aHR0cHM6Ly9jZG4uanNkZWxpdnIubmV0L2doL3NpbmRyZXNvcmh1cy9kZXZ0b29scy1kZXRlY3QvaW5kZXgubWluLmpz");
-document.appendChild(newScript);
+document.getElementsByTagName('body')[0].appendChild(newScript);
 window.addEventListener('devtoolschange', event => {
 if (event.detail.isOpen) {window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"};
 });
