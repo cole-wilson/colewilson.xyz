bd36063fa7eac09c3b4e2423ba7de4f0d40e001b
Cole Wilson <cole@colewilson.xyz>
2021-03-18 11:58:56 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 ---
 ---
 {% capture stylesheet_url %}{% include styles.html %}{% endcapture %}
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = `offline.{{ stylesheet_url | replace: '<link rel="stylesheet" href="https://colewilson.xyz/assets/','' | replace: '.min.css"></link>','' }}`;
+const OFFLINE_VERSION = 2;
+const CACHE_NAME = `offline.${OFFLINE_VERSION}.{{ stylesheet_url | replace: '<link rel="stylesheet" href="https://colewilson.xyz/assets/','' | replace: '.min.css"></link>','' }}`;
 const OFFLINE_URL = "/offline/";
 
 self.addEventListener('install', (event) => {
@@ -18,6 +18,9 @@ self.addEventListener('install', (event) => {
 
 this.addEventListener('fetch', event => {
     if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        if (event.request.url == 'easterbunny.js') {
+            console.log("HA!!!)
+        }
         event.respondWith(
             fetch(event.request.url).catch(error => {
                 return caches.match(OFFLINE_URL);
