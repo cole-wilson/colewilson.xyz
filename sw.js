a5a8141d98df5d286c4bc4fad583d5333adeca2a
Cole Wilson <cole@colewilson.xyz>
2021-03-18 13:39:38 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,20 @@
 ---
 ---
 {% capture stylesheet_url %}{% include styles.html %}{% endcapture %}
-{% capture scripts %}{% include scripts.html %}{% endcapture %}
+{% capture script_tags %}{% include scripts.html %}{% endcapture %}
+{% assign scripts = script_tags | split: '"></script>' %}
+
 
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = `offline.${OFFLINE_VERSION}.{{ stylesheet_url | replace: '<link rel="stylesheet" href="https://colewilson.xyz/assets/','' | replace: '.min.css"></link>','' }}`;
 const OFFLINE_URL = "/offline/";
 const POSTS = [{% for post in site.posts %}"{{ post.url }}", {% endfor %}];                                     
 const PAGES = [{% for page in site.pages %}"{{ page.url }}", {% endfor %}];
-const ASSETS = ["https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js", "https://unpkg.com/clippyjs@latest", "{{scripts}}"]
+const ASSETS = [
+{%- for script in scripts -%}
+    "{{ script | replace: '<script src="', "" }}",
+{% endfor -%}
+]
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
